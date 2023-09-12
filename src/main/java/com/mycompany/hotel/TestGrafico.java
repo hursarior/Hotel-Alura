77c73090d4a647d2957395944a6f200c7492ff24
James <Jamesronaldoperez@gmail.com>
2023-09-12 13:54:03 -0400

    arreglando error

--- src/main/java/com/mycompany/hotel/TestGrafico.java
+++ src/main/java/com/mycompany/hotel/TestGrafico.java
@@ -1,6 +1,7 @@
 package com.mycompany.hotel;
 
 public class TestGrafico {
+    public static void main(String[] args) {
       MnuPrincipal in = new MnuPrincipal();
       in.setVisible(true);
       
